ec7c4a3648179b92452e527d6ba5da00d4aac052
yuyan <yuyan1991@gmail.com>
2017-12-18 11:02:45 +0800

    Add two solution code for ural2001

---
+++ ural2001.py
@@ -0,0 +1,7 @@
+# -*- config: utf-8 -*-
+
+a = input().split()
+b = input().split()
+c = input().split()
+
+print(int(a[0])-int(c[0]),int(a[1])-int(b[1]))
---
+++ ural2001_1.py
@@ -0,0 +1,7 @@
+# -*- config: utf-8 -*-
+
+a1,b1 = [int(num) for num in input().split()]
+a2,b2 = [int(num) for num in input().split()]
+a3,b3 = [int(num) for num in input().split()]
+
+print(a1-a3,b1-b2)
